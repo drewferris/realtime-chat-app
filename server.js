c2aa6aa590e5c89da565dc223018956c74e973ac
Drew Ferris <ddf8029@gmail.com>
2020-07-24 18:24:06 -0700

    app complete

--- server.js
+++ server.js
@@ -14,31 +14,55 @@ const handler = app.getRequestHandler();
 const sentiment = new Sentiment();
 
 const pusher = new Pusher({
-	appId: process.env.PUSHER_APP_ID,
-	key: process.env.PUSHER_APP_KEY,
-	secret: process.env.PUSHER_APP_SECRET,
-	cluster: process.env.PUSHER_APP_CLUSTER,
-	encrypted: true,
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+    encrypted: true,
 });
 
 app.prepare()
-	.then(() => {
-		const server = express();
+    .then(() => {
+        const server = express();
 
-		server.use(cors());
-		server.use(bodyParser.json());
-		server.use(bodyParser.urlencoded({ extended: true }));
+        server.use(cors());
+        server.use(bodyParser.json());
+        server.use(bodyParser.urlencoded({ extended: true }));
 
-		server.get('*', (req, res) => {
-			return handler(req, res);
-		});
+        server.get('*', (req, res) => {
+            return handler(req, res);
+        });
 
-		server.listen(port, (err) => {
-			if (err) throw err;
-			console.log(`> Ready on http://localhost:${port}`);
-		});
-	})
-	.catch((ex) => {
-		console.log(ex.stack);
-		process.exit(1);
-	});
+        const chatHistory = { messages: [] };
+
+        server.post('/message', (req, res, next) => {
+            const {
+                user = null,
+                message = '',
+                timestamp = +new Date,
+            } = req.body;
+            const sentimentScore = sentiment.analyze(message).score;
+            const chat = {
+                user,
+                message,
+                timestamp,
+                sentiment: sentimentScore,
+            };
+
+            chatHistory.messages.push(chat);
+            pusher.trigger('chat-room', 'new-message', { chat });
+        });
+
+        server.post('/messages', (req, res, next) => {
+            res.json({ ...chatHistory, status: 'success' });
+        });
+
+        server.listen(port, err => {
+            if (err) throw err;
+            console.log(`> Ready on http://localhost:${port}`);
+        });
+    })
+    .catch(ex => {
+        console.log(ex.stack);
+        process.exit(1);
+    });
